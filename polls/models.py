7e802cea39652abdcc0005dfecdfdcf9e995b20c
jmgstokes <35575742+jmgstokes@users.noreply.github.com>
2018-04-05 10:26:41 -0600

    Add files via upload

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,16 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from operator import attrgetter
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def get_choices(self):
         """Returns a list of choice objects associated with this question."""
@@ -19,11 +22,15 @@ class Question(models.Model):
 
     def get_leading_choice(self):
         """Returns the choice object with the most votes."""
-        pass  # Requires implementation!
+        choice_list = list(self.choice_set.all())
+
+        max_choice = max(choice_list.key=attrgetter('votes'))
+
+        return max_choice
 
     def get_leading_choice_pct(self):
         """Returns the percentage of votes for the leading choice."""
-        total_votes = 1.0
+        total_votes = 0.0
         for choice in self.get_choices():
             total_votes += votes
         
--- polls/tests.py
+++ polls/tests.py
@@ -11,9 +11,61 @@ from .models import Question, Choice
 
 class QuestionModelTests(TestCase):
 
-    fixtures = ['polls_testdata.json']
+	fixtures = ['polls_testdata.json']
 
-    def test_fixture(self):
-        self.assertEqual(Question.objects.all().count(), 1)
-        self.assertEqual(Choice.objects.all().count(), 6)
+	def test_fixture(self):
+		self.assertEqual(Question.objects.all().count(), 1)
+		self.assertEqual(Choice.objects.all().count(), 6)
 
+	def test_was_published_recently_with_future_question(self):
+		"""
+		returns False for questions whose pub_date is in the future
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+
+		self.assertIs(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		"""
+		was_published_recently() returns False for questions whose pub_date is older than 1 day
+		"""
+		time = timezone.now() - datetime.timedelta(days=1,seconds=1)
+		old_question = Question(pub_date=time)
+		self.assertIs(old_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_question(self):
+		"""
+		was_published_recently() returns True for questions whose pub_date is within the last day
+		"""
+		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+		recent_question = Question(pub_date=time)
+		self.assertEqual(recent_question.was_published_recently(), True)
+
+
+	def test_get_choices(self):
+		"""
+		returns True if number of choices is six
+		"""
+		question_object = Question.objects.get(pk=2)
+		choices = question_object.get_choices()
+		self.assertEqual(len(choices), 6)
+		self.assertEqual
+
+
+	def test_leading_choice(self):
+		"""
+		returns True if the leading choice is Mickey Mouse
+		"""
+		question_object = Question.objects.get(pk=2)
+		leader = question_object.get_leading_choice()
+		self.assertEqual(leader.choice_text, 'Mickey Mouse')
+
+
+	def test_leading_choice_pct(self):
+		"""
+		returns True if the percentage of leading votes is .3125
+		"""
+		question_object = Question.objects.get(pk=2)
+		percent = question_object.get_leading_choice_pct()
+		self.assertEqual(percent, 0.3125)
